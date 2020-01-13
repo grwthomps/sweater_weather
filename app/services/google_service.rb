e4f758701e26b79c092ac685526a4d1640577833
Graham Thompson <grwthomps@gmail.com>
2020-01-13 11:27:05 -0700

    Refactor google service by breaking out faraday connections

--- app/services/google_service.rb
+++ app/services/google_service.rb
@@ -1,8 +1,7 @@
 class GoogleService
   def fetch_lat_long(location)
-    response = Faraday.get('https://maps.googleapis.com/maps/api/geocode/json') do |req|
+    response = conn.get do |req|
       req.params['address'] = location
-      req.params['key'] = ENV['google_api_key']
     end
 
     JSON.parse(response.body, symbolize_names: true)
@@ -11,11 +10,19 @@ class GoogleService
   def fetch_city(lat,lng)
     latlng = "#{lat},#{lng}"
 
-    response = Faraday.get('https://maps.googleapis.com/maps/api/geocode/json') do |req|
+    response = conn.get do |req|
       req.params['latlng'] = latlng
-      req.params['key'] = ENV['google_api_key']
     end
 
     JSON.parse(response.body, symbolize_names: true)
+  end
+
+  private
+
+  def conn
+    Faraday.new(
+      url: 'https://maps.googleapis.com/maps/api/geocode/json',
+      params: {'key' => ENV['google_api_key']}
+    )
   end
 end
