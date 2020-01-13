a02d8b02bdd57523c77537bd1cb41b7696954a4b
Graham Thompson <grwthomps@gmail.com>
2020-01-13 10:12:12 -0700

    Update antipode facade to utilize amypode service, google service, and antipode poro

--- app/facades/antipode_facade.rb
+++ app/facades/antipode_facade.rb
@@ -5,10 +5,16 @@ class AntipodeFacade
 
     lat = geolocation_data[:results][0][:geometry][:location][:lat]
     lng = geolocation_data[:results][0][:geometry][:location][:lng]
+    origin_city = geolocation_data[:results][0][:formatted_address]
 
     dark_sky_service = DarkSkyService.new
     forecast_data = dark_sky_service.fetch_forecast(lat,lng)
 
-    binding.pry
+    amypode_service = AmypodeService.new
+    antipode_city_coord = amypode_service.fetch_antipode_city(lat,lng)[:data][:attributes]
+
+    antipode_city = google_service.fetch_city(antipode_city_coord[:lat], antipode_city_coord[:long])[:results][0][:formatted_address]
+
+    Antipode.new(origin_city, antipode_city, forecast_data)
   end
 end
