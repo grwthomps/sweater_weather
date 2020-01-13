102f32a8d4a6431e30939c392c33dda768ffb14d
Graham Thompson <grwthomps@gmail.com>
2020-01-13 10:10:30 -0700

    Update google service with fetch_city method

--- app/services/google_service.rb
+++ app/services/google_service.rb
@@ -7,4 +7,15 @@ class GoogleService
 
     JSON.parse(response.body, symbolize_names: true)
   end
+
+  def fetch_city(lat,lng)
+    latlng = "#{lat},#{lng}"
+
+    response = Faraday.get('https://maps.googleapis.com/maps/api/geocode/json') do |req|
+      req.params['latlng'] = latlng
+      req.params['key'] = ENV['google_api_key']
+    end
+
+    JSON.parse(response.body, symbolize_names: true)
+  end
 end
