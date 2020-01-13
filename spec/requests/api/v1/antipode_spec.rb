8307568ca8dde42573a9f24042e9b0fa17a1ee11
Graham Thompson <grwthomps@gmail.com>
2020-01-13 09:32:20 -0700

    Create test for antipode endpoint

---
+++ spec/requests/api/v1/antipode_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe 'API V1 Antipode', :vcr, type: 'request' do
+  describe "GET /api/v1/antipode" do
+    it 'can return antipode info' do
+      get '/api/v1/antipode?location=hongkong'
+
+      expect(response).to be_successful
+
+      antipode = JSON.parse(response.body)
+      expect(forecast["data"]["attributes"]).not_to be_empty
+    end
+  end
+end
