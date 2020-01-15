84d7aefd772c0ad03f95ca8a65a07da5351cff2f
Graham Thompson <grwthomps@gmail.com>
2020-01-15 11:51:26 -0700

    Refactor controllers to use fewer variables and lines

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::BackgroundsController < ApplicationController
   def index
-    facade = BackgroundFacade.new
-    background = facade.retrieve_background(params[:location])
+    background = BackgroundFacade.retrieve_background(params[:location])
     render json: BackgroundSerializer.new(background)
   end
 end
--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,11 +1,15 @@
 class Api::V1::RoadTripController < ApplicationController
   def create
     user = User.find_by(api_key: params[:api_key])
-    if user
-      trip = RoadTripFacade.calculate_trip(params[:origin], params[:destination])
-      render json: RoadTripSerializer.new(trip)
+
+    return render json: {error: "Invalid api key."}, status: 401 if user.nil?
+
+    trip = RoadTripFacade.calculate_trip(params[:origin], params[:destination])
+
+    if trip.nil?
+      render json: {error: "Invalid request or no results found."}, status: 400
     else
-      render json: {error: "Invalid api key."}, status: 401
+      render json: RoadTripSerializer.new(trip)
     end
   end
 end
