d0800877cbcd8dbc06e96fad3e91ddc65b022718
Graham Thompson <grwthomps@gmail.com>
2020-01-14 20:06:59 -0700

    Refactor road trip endpoint test

--- spec/requests/api/v1/road_trip_spec.rb
+++ spec/requests/api/v1/road_trip_spec.rb
@@ -1,7 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe 'API V1 Road Trip', :vcr, type: 'request' do
+RSpec.describe 'API V1 Road Trip', type: 'request' do
   describe "POST /api/v1/road_trip" do
+    WebMock.enable_net_connect!
+    VCR.eject_cassette
+    VCR.turn_off!(ignore_cassettes: true)
+
     before(:each) do
       @user = User.create(email: "joe@example.com", password: "password", password_confirmation: "password")
     end
@@ -16,9 +20,13 @@ RSpec.describe 'API V1 Road Trip', :vcr, type: 'request' do
 
     it 'can return road trip info' do
       post '/api/v1/road_trip', params: valid_params
-
       expect(response).to have_http_status(200)
-      expect(response.body["data"]["attributes"]).not_to be_empty
+
+      trip = JSON.parse(response.body)
+
+      expect(trip["data"]["attributes"]).not_to be_empty
+      expect(trip["data"]["attributes"]["origin"]).to eq("Denver,CO")
+      expect(trip["data"]["attributes"]["destination"]).to eq("Pueblo,CO")
     end
   end
 end
