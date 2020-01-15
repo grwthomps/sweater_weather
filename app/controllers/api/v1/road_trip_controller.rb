0cf3d1dd6808183257606ec2286afd48c58772f9
Graham Thompson <grwthomps@gmail.com>
2020-01-14 20:09:27 -0700

    Refactor road trip controller create action

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,5 +1,11 @@
 class Api::V1::RoadTripController < ApplicationController
   def create
-    # binding.pry
+    user = User.find_by(api_key: params[:api_key])
+    if user
+      trip = RoadTripFacade.calculate_trip(params[:origin], params[:destination])
+      render json: RoadTripSerializer.new(trip)
+    else
+      render json: {error: "Invalid api key."}, status: 401
+    end
   end
 end
