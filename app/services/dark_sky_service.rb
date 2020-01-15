fc668191de105bd1ed02ee7e2067009df0541cfe
Graham Thompson <grwthomps@gmail.com>
2020-01-14 20:09:08 -0700

    Add fetch_future_forecast to dark sky service

--- app/services/dark_sky_service.rb
+++ app/services/dark_sky_service.rb
@@ -1,9 +1,27 @@
 class DarkSkyService
   def fetch_forecast(lat,lng)
-    response = Faraday.get("https://api.darksky.net/forecast/#{ENV['dark_sky_api_key']}/#{lat},#{lng}") do |req|
+    response = conn.get("#{lat},#{lng}") do |req|
       req.params['exclude'] = 'minutely,alert,flags'
     end
 
+    parse_response(response)
+  end
+
+  def fetch_future_forecast(lat, lng, time)
+    response = conn.get("#{lat},#{lng},#{time}") do |req|
+      req.params['exclude'] = 'daily,hourly,minutely,alert,flags'
+    end
+
+    parse_response(response)
+  end
+
+  private
+
+  def conn
+    Faraday.new(url: "https://api.darksky.net/forecast/#{ENV['dark_sky_api_key']}")
+  end
+
+  def parse_response(response)
     JSON.parse(response.body, symbolize_names: true)
   end
 end
