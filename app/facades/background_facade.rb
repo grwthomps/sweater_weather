1a4144ac06e98baa9463a6c95f4b017ff8e94a0a
Graham Thompson <grwthomps@gmail.com>
2020-01-15 09:30:31 -0700

    Create background facade with retrieve_background method

---
+++ app/facades/background_facade.rb
@@ -0,0 +1,12 @@
+class BackgroundFacade
+  def retrieve_background(location)
+    service = UnsplashService.new
+    background_url = service.fetch_background(location)[:results]
+    if background_url.empty?
+      background_url = "https://images.unsplash.com/photo-1546931665-73db142e716f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1867&q=80"
+      Background.new(background_url)
+    else
+      Background.new(background_url[0][:urls][:raw])
+    end
+  end
+end
